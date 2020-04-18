7284785623c97661162cd3be5fd19bc9e7dba3de
luanaselena <52636725+luanaselena@users.noreply.github.com>
2020-04-18 18:26:46 -0300

    projecto

---
+++ src/test/java/com/unla/oo2DemocratizacionStock/Oo2DemocratizacionStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.oo2DemocratizacionStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oo2DemocratizacionStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
