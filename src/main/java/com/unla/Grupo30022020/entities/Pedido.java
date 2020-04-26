6a46b075cad787c0448c5d8e7fefb60776604067
nicolasborea <nicolasborea@gmail.com>
2020-04-26 17:01:21 -0300

    entidades nico

---
+++ src/main/java/com/unla/Grupo30022020/entities/Pedido.java
@@ -0,0 +1,71 @@
+package com.unla.Grupo30022020.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Pedido {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idPedido;
+	
+	@Column
+	private int cantidad;
+	
+	@Column
+	private boolean aceptado;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "productoId", referencedColumnName = "id")
+	private Producto producto;	
+	
+	public Pedido() {}
+
+	public Pedido(long idPedido, int cantidad, boolean aceptado, Producto producto) {
+		super();
+		this.idPedido = idPedido;
+		this.cantidad = cantidad;
+		this.aceptado = aceptado;
+		this.producto = producto;
+	}
+
+	public long getIdPedido() {
+		return idPedido;
+	}
+
+	protected void setIdPedido(long idPedido) {
+		this.idPedido = idPedido;
+	}
+
+	public int getCantidad() {
+		return cantidad;
+	}
+
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+
+	public boolean isAceptado() {
+		return aceptado;
+	}
+
+	public void setAceptado(boolean aceptado) {
+		this.aceptado = aceptado;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+	
+	
+}
---
+++ src/main/java/com/unla/Grupo30022020/entities/Sucursal.java
@@ -0,0 +1,106 @@
+package com.unla.Grupo30022020.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Sucursal {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idSucursal;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "ubicacionId", referencedColumnName = "id")
+	private Direccion ubicacion;
+	
+	@Column
+	private long telefono;
+	
+//	@OneToOne(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "gerenteId", referencedColumnName = "id")
+//	private Gerente gerente;
+//
+//	@OneToMany(cascade = CascadeType.ALL)
+//	@JoinColumn(name = "vendedorId", referencedColumnName = "id")
+//	private Set<Vendedor> vendedores;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "sucursal")
+	@JoinColumn(name = "loteId", referencedColumnName = "id")
+	private Set<Lote> lotes;
+	
+	public Sucursal() {}
+
+	public Sucursal(long idSucursal, Direccion ubicacion, long telefono
+//			Gerente gerente, Set<Vendedor> vendedores,
+//			Set<Lote> lotes
+			) {
+		super();
+		this.idSucursal = idSucursal;
+		this.ubicacion = ubicacion;
+		this.telefono = telefono;
+//		this.gerente = gerente;
+//		this.vendedores = vendedores;
+//		this.lotes = lotes;
+	}
+
+	public long getIdSucursal() {
+		return idSucursal;
+	}
+
+	public void setIdSucursal(long idSucursal) {
+		this.idSucursal = idSucursal;
+	}
+
+	public Direccion getUbicacion() {
+		return ubicacion;
+	}
+
+	public void setUbicacion(Direccion ubicacion) {
+		this.ubicacion = ubicacion;
+	}
+
+	public long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(long telefono) {
+		this.telefono = telefono;
+	}
+
+//	public Gerente getGerente() {
+//		return gerente;
+//	}
+//
+//	public void setGerente(Gerente gerente) {
+//		this.gerente = gerente;
+//	}
+//
+//	public Set<Vendedor> getVendedores() {
+//		return vendedores;
+//	}
+//
+//	public void setVendedores(Set<Vendedor> vendedores) {
+//		this.vendedores = vendedores;
+//	}
+
+//	public Set<Lote> getLotes() {
+//		return lotes;
+//	}
+//
+//	public void setLotes(Set<Lote> lotes) {
+//		this.lotes = lotes;
+//	}
+//	
+	
+}
